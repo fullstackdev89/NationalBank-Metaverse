9debbbd88832f8eb28693e6722eac64e7e889349
fullstackdev89 <fullstackdev1989@gmail.com>
2022-08-24 10:12:19 +0900

    Update Frontend

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App() {
             <p className="cursor-pointer">Integrations</p>
             <p className="cursor-pointer">Contact Us</p>
           </div>
-          <div className="sm:mt-20">
-            <div className="sm:flex justify-between gap-10 items-center">
+          <div className="sm:mt-10">
+            <div className="sm:flex justify-between gap-10 items-start">
               <div className="sm:w-1/2">
-                <p className="text-white text-center sm:text-start text-3xl sm:text-6xl font-bold">
+                <p className="text-white text-center sm:text-start text-3xl sm:text-6xl font-bold mt-0 sm:mt-32">
                   First National Bank
                   <p className="text-green-300 mt-3 text-2xl sm:text-5xl">
                     of the METAVERSE
@@ -24,13 +24,13 @@ function App() {
                   Digital world you can converse, earn, merge real and virtual
                   assets.
                 </p>
-                <div className="bg-white text-black mt-6 sm:mt-10 flex justify-between items-center px-2">
+                <div className="bg-white text-black mt-6 sm:mt-10 flex justify-between items-center px-2 rounded">
                   <input
                     type={"text"}
                     className="bg-transparent outline-0 h-14 sm:h-16 px-4 w-full"
                     placeholder="Enter your wallet address"
                   />
-                  <button className="bg-black text-white text-md sm:text-lg font-medium h-10 sm:h-12 w-[200px]">
+                  <button className="bg-black text-white text-md sm:text-lg font-medium h-10 sm:h-12 w-[200px] rounded">
                     Connect Wallet
                   </button>
                 </div>
