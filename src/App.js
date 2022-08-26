29c9c62c96a2e02029dfb5df2b7df3e33115cf16
fullstackdev89 <fullstackdev1989@gmail.com>
2022-08-27 01:08:42 +0900

    Update Coming Soon

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import "./App.css";
 function App() {
   return (
     <div className="">
-      <div className="bg-[#212121] w-full h-full min-h-screen">
+      <div className="py-4">
+        <div className="max-w-[1280px] w-11/12 m-auto sm:flex justify-between items-center">
+          <a
+            href="https://www.Block2AI.com"
+            target="_blank"
+            without
+            rel="noreferrer"
+          >
+            <img src="/img/logo.png" alt="" className="m-auto sm:m-0" />
+          </a>
+          <img
+            src="/img/coming.png"
+            alt=""
+            className=" m-auto sm:mx-0 -mb-40 max-w-8/12"
+          />
+        </div>
+      </div>
+      <div className="bg-[#212121] w-full h-full">
         <div className="max-w-[1280px] w-11/12 m-auto">
           {/* <div className="flex justify-center py-10 items-center gap-3 sm:gap-20 text-white font-medium text-md sm:text-2xl">
             <p className="cursor-pointer">Products</p>
@@ -14,15 +31,20 @@ function App() {
           <div className="">
             <div className="sm:flex justify-between gap-10 items-start pt-20">
               <div className="sm:w-1/2">
-                <p className="text-white text-center sm:text-start text-3xl sm:text-8xl mt-0 sm:mt-32 font-bold">
-                  FINANCE & BANKING
-                  <p className="text-green-300 mt-3 text-2xl sm:text-5xl font-bolder">
-                    3D ICONS
+                <p className="text-white text-center sm:text-start text-3xl sm:text-6xl font-bold pt-20 sm:pt-0">
+                  First National Bank
+                  <p className="text-[#39D995] mt-3 text-2xl sm:text-5xl font-bolder">
+                    of the METAVERSE
                   </p>
                 </p>
-                <div className="py-5 mt-20 w-max px-10 rounded-r-full bg-white/25 flex justify-center items-center">
+                <p className="text-white text-2xl sm:text-start text-center mt-5">
+                  Digital world you can converse, earn, merge real and virtual
+                  assets.
+                </p>
+                <img src="/img/8.png" alt="" />
+                {/* <div className="py-5 mt-20 w-max px-10 rounded-r-full bg-white/25 flex justify-center items-center">
                   <p className="text-white text-4xl font-bold">DEMO</p>
-                </div>
+                </div> */}
                 {/* <p className="text-white mt-3 sm:mt-10 text-center sm:text-start text-lg sm:text-3xl">
                   Digital world you can converse, earn, merge real and virtual
                   assets.
